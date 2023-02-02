44d597762152bed5e652a41252d9f4b5a3044b79
Willy <willy@coinerella.com>
2023-02-02 18:20:38 +0100

    basic version

--- src/app.tsx
+++ src/app.tsx
@@ -3,42 +3,80 @@ import Progress from "react-circle-progress-bar";
 
 export function App() {
   const [progress, setProgress] = useState(0.0);
+  const [isOver90Percent, setIsOver90Percent] = useState(false);
+  const activationTimeReached = Date.now() >= 1681732800 * 1000;
+
   useEffect(() => {
     async function fetchProgress() {
       const response = await fetch(
         "https://fork.peercoinexplorer.net/current.dat"
       );
       const text = await response.text();
-      setProgress(parseFloat(text.split(",")[1]));
+      const progressInBackend = parseFloat(text.split(",")[1]);
+      setProgress(progressInBackend);
+      setIsOver90Percent(progressInBackend >= 90 ? true : false);
     }
     fetchProgress();
   }, []);
 
   return (
     <div class="ppc-hero">
-      <div class="flex-center m-t-3">
-        <img
-          class="ppc-logo"
-          src="img/peercoin-horizontal-greenleaf-blacktext-transparent.svg"
-          alt="peercoin logo"
-        />
-      </div>
-      <h1>0.12 Fork Watcher</h1>
-      <div class="ppc-background">
-        <div class="flex-center m-y-3">
-          <Progress
-            progress={progress}
-            // gradient={[
-            //   { stop: 0.0, color: "#3cb054" },
-            //   { stop: 1, color: "#3cb054" },
-            // ]}
+      <div class="m-y-1">
+        <div class="flex-center ppc-logo-container">
+          <img
+            class="ppc-logo"
+            src="img/peercoin-horizontal-greenleaf-blacktext-transparent.svg"
+            alt="peercoin logo"
           />
         </div>
-        <div class="block-center">
-          <div>- 90% majority block version 4 over the last 1000 blocks </div>
-          <div>- April 17th, 2023 - 12:00 UTC reached</div>
+        <h1>0.12 Fork Watcher</h1>
+      </div>
+      <div class="ppc-background">
+        <div class="flex-center m-y-1">
+          <Progress
+            progress={progress}
+            background="white"
+            className="ppc-progress-circle"
+            subtitle="Blocks v4"
+            gradient={[
+              { stop: 0.0, color: "black" },
+              { stop: 1, color: "grey" },
+            ]}
+          />
         </div>
-        <div class="m-y-3">chart</div>
+        <div>
+          <table class="ppc-table">
+            <tr>
+              <td>
+                <img
+                  src={isOver90Percent ? "img/check.png" : "img/cross.png"}
+                  alt="status"
+                />
+              </td>
+              <td>90% majority block version 4 over the last 1000 blocks</td>
+            </tr>
+            <tr>
+              <td>
+                <img
+                  src={
+                    activationTimeReached ? "img/check.png" : "img/cross.png"
+                  }
+                  alt="status"
+                />
+              </td>
+              <td>April 17th, 2023 - 12:00 UTC reached</td>
+            </tr>
+          </table>
+          <div class="block-center">
+            {isOver90Percent && activationTimeReached ? (
+              <h2>Fork activated</h2>
+            ) : (
+              <h2>Fork not activated</h2>
+            )}
+          </div>
+        </div>
+
+        {/* <div class="m-y-3">chart</div> */}
       </div>
     </div>
   );
