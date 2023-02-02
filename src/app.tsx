931c400d47fb55497b351afe4eb26a99279831e5
Willy <willy@coinerella.com>
2023-02-02 17:24:33 +0100

    basic layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,29 +1,45 @@
 import { useState, useEffect } from "preact/hooks";
-import "./app.css";
 import Progress from "react-circle-progress-bar";
 
 export function App() {
-  const [data, setData] = useState(0.0);
+  const [progress, setProgress] = useState(0.0);
   useEffect(() => {
-    async function fetchData() {
+    async function fetchProgress() {
       const response = await fetch(
         "https://fork.peercoinexplorer.net/current.dat"
       );
       const text = await response.text();
-      setData(parseFloat(text.split(",")[1]));
+      setProgress(parseFloat(text.split(",")[1]));
     }
-    fetchData();
+    fetchProgress();
   }, []);
 
   return (
-    <div class="progress-container">
-      <Progress
-        progress={data}
-        gradient={[
-          { stop: 0.0, color: "#3cb054" },
-          { stop: 1, color: "#3cb054" },
-        ]}
-      />
+    <div class="ppc-hero">
+      <div class="flex-center m-t-3">
+        <img
+          class="ppc-logo"
+          src="img/peercoin-horizontal-greenleaf-blacktext-transparent.svg"
+          alt="peercoin logo"
+        />
+      </div>
+      <h1>0.12 Fork Watcher</h1>
+      <div class="ppc-background">
+        <div class="flex-center m-y-3">
+          <Progress
+            progress={progress}
+            // gradient={[
+            //   { stop: 0.0, color: "#3cb054" },
+            //   { stop: 1, color: "#3cb054" },
+            // ]}
+          />
+        </div>
+        <div class="block-center">
+          <div>- 90% majority block version 4 over the last 1000 blocks </div>
+          <div>- April 17th, 2023 - 12:00 UTC reached</div>
+        </div>
+        <div class="m-y-3">chart</div>
+      </div>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { render } from "preact";
 import { App } from "./app";
 import "./scss/index.scss";
-import "./scss/google-fonts-roboto.css";
 
 render(<App />, document.getElementById("app") as HTMLElement);
